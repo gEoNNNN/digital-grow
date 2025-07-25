1dd925b0798edac1af241fda8f3f41f61f1a2f46
Eugen Ostafi <eugen.ostafi@isa.utm.md>
2025-07-25 22:53:59 +0300

    :zap: new page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import ServicesPage from './pages/ServicesPage'
 import Portfolio from './pages/Potrfolio'
 import AboutUsPage from './pages/AboutUsPage'
+import ContactsPage from './pages/ContactsPage'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/services" element={<ServicesPage />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/aboutus" element={<AboutUsPage />} />
+          <Route path="/contacts" element={<ContactsPage />} />
         </Routes>
       </div>
     </Router>
